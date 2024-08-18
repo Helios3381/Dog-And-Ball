8bbb38a6d301c4ced7ff6714795766b5c8f89c75
Helios3381 <helioshoiming@gmail.com>
2024-08-18 13:40:21 +0800

    First commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float delay = 1f;
+    private Boolean canPress = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canPress)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canPress = false;
+            Invoke("EnablePress", delay);
+        }
+    }
+
+    void EnablePress()
+    {
+        canPress = true;
+    }
+}
